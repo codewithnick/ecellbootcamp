2be4e739b2b8d929f117abde6f05cb34edc42949
Aman Agrahari <104276710+AmanAgrahariaa@users.noreply.github.com>
2023-06-19 22:33:25 +0530

    aman's commit

---
+++ app.js
@@ -0,0 +1,17 @@
+window.addEventListener("click", function (e) {
+    removemodel(e);
+});
+function removemodel(e) {
+    let model = document.getElementById('player');
+    if (e.target == model) {
+        document.getElementById('player').style.display = 'none';
+        document.getElementById('iframe').src = ``;
+    };
+}
+function playvideo(link) {
+    document.getElementById('player').style.display = "block";
+    document.getElementById('iframe').src = `https://www.youtube.com/embed/${link}`;
+}
+
+
+  
